53debfd659761bcb5c10f2d735468bbc6407ad01
Lars Nieuwenhuizen <l.nieuwenhuizen@fullstaq.com>
2023-09-13 09:49:27 +0000

    fix: Missing test file

--- test/neovim/neovim.sh
+++ test/neovim/neovim.sh
@@ -5,6 +5,7 @@ set -e
 # Optional: Import test library bundled with the devcontainer CLI
 source dev-container-features-test-lib
 
+# Feature-specific tests
 check "neovim" nvim --version
 
 # Report result
